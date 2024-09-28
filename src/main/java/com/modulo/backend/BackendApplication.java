4f59fc6077b18b93bfdd993e63e59e338df2e904
kamilo9809 <camilorivera09@outlook.com>
2024-09-27 23:03:15 -0500

    feature/feature/prueba1/esto es una prueba

--- src/main/java/com/modulo/backend/BackendApplication.java
+++ src/main/java/com/modulo/backend/BackendApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("hola mundo");
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
