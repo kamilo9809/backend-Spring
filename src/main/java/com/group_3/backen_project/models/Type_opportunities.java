0edb29357b5692d8601d3ae8aa68f838078a54a0
kamilo9809 <camilorivera09@outlook.com>
2024-10-26 09:13:17 -0500

    add feature type oportunidades

--- src/main/java/com/group_3/backen_project/models/Type_opportunities.java
+++ src/main/java/com/group_3/backen_project/models/Type_opportunities.java
@@ -8,4 +8,7 @@ public class Type_opportunities {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "oportunity_type")
+    private String oportunityType;
 }
