370447310c17c4576d8b38ab0f016a239cd947a6
kamilo9809 <camilorivera09@outlook.com>
2024-10-27 09:15:41 -0500

    feat: :sparkles: add status review

--- src/main/java/com/group_3/backen_project/models/Status_institutions.java
+++ src/main/java/com/group_3/backen_project/models/Status_institutions.java
@@ -8,4 +8,7 @@ public class Status_institutions {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "status_review")
+    private String statusReview;
 }
