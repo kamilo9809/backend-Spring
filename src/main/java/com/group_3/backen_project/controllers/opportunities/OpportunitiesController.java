afebd9b159aef019589e15a52740d8a037c1dc43
kamilo9809 <camilorivera09@outlook.com>
2024-10-27 12:09:15 -0500

    feat: :sparkles: add controller opportunities

--- src/main/java/com/group_3/backen_project/controllers/opportunities/OpportunitiesController.java
+++ src/main/java/com/group_3/backen_project/controllers/opportunities/OpportunitiesController.java
@@ -1,4 +1,61 @@
 package com.group_3.backen_project.controllers.opportunities;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.group_3.backen_project.controllers.opportunities.service.OpportunitiesServices;
+import com.group_3.backen_project.models.Opportunities;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("api/opportunities")
 public class OpportunitiesController {
+
+    @Autowired
+    private OpportunitiesServices opportunitiesServices;
+
+    @PostMapping("/{idCategories}/{idStatus}/{idType}")
+    public ResponseEntity<Opportunities> postOpportunities(
+            @RequestBody Opportunities opportunities,
+            @PathVariable Long idCategories,
+            @PathVariable Long idStatus,
+            @PathVariable Long idType) {
+
+        return ResponseEntity
+                .ok(opportunitiesServices.createOpportunities(opportunities, idCategories, idStatus, idType));
+    }
+
+    @GetMapping
+    public List<Opportunities> getOpportunities() {
+        return opportunitiesServices.getOpportunitiesAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Opportunities> getOportunity(@PathVariable Long id) {
+        Optional<Opportunities> opportunity = opportunitiesServices.getOpportunitiesById(id);
+        return opportunity.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Opportunities> putOpportunities(@PathVariable Long id,
+            @RequestBody Opportunities detailOpportunities) {
+        return ResponseEntity.ok(opportunitiesServices.updateOpportunities(id, detailOpportunities));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        opportunitiesServices.deleteOpportunities(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/group_3/backen_project/models/Bootcamps.java
+++ src/main/java/com/group_3/backen_project/models/Bootcamps.java
@@ -3,9 +3,11 @@ package com.group_3.backen_project.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
 @Table(name = "bootcamps")
+@Data
 public class Bootcamps {
 
     @Id
@@ -13,99 +15,23 @@ public class Bootcamps {
     private Long id;
 
     @NotBlank(message = "El idGeneral es obligatorio")
-    @ManyToOne
-    private Long idGeneral;
-
-    @Column(nullable = true)
-    @Size(max = 500, message = "La descripcion no puede exceder 500 caracteres")
-    private String descripcion;
+    @OneToOne
+    private Descriptions_bootcamps idGeneral;
 
     @NotBlank(message = "El idTemas es obligatorio")
-    @ManyToOne
-    private Long idTemas;
+    @OneToOne
+    private Topics_bootcamps idTemas;
 
-    @NotBlank(message = "El nombre es obligatorio") //Valida que no esté vacío
+    @NotBlank(message = "El nombre es obligatorio")
     @Size(max = 100, message = "El nombre no puede exceder 100 caracteres")
     @Column(nullable = false, length = 100)
     private String nombre;
 
     @NotBlank(message = "El idCostos es obligatorio")
-    @ManyToOne
-    private Long idCostos;
+    @OneToOne
+    private Costs_bootcamps idCostos;
 
     @NotBlank(message = "El idEstado es obligatorio")
-    @ManyToOne
-    private Long idEstado;
-
-    // Constructores
-    public Bootcamps() {
-    }
-
-    public Bootcamps(Long idGeneral, String descripcion, Long idTemas, String nombre, Long idCostos, Long idEstado) {
-        this.idGeneral = idGeneral;
-        this.descripcion = descripcion;
-        this.idTemas = idTemas;
-        this.nombre = nombre;
-        this.idCostos = idCostos;
-        this.idEstado = idEstado;
-    }
-
-    // Getters y Setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Long getIdGeneral() {
-        return idGeneral;
-    }
-
-    public void setIdGeneral(Long idGeneral) {
-        this.idGeneral = idGeneral;
-    }
-
-    public Long getIdTemas() {
-        return idTemas;
-    }
-
-    public void setIdTemas(Long idTemas) {
-        this.idTemas = idTemas;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Long getIdCostos() {
-        return idCostos;
-    }
-
-    public void setIdCostos(Long idCostos) {
-        this.idCostos = idCostos;
-    }
-
-    public Long getIdEstado() {
-        return idEstado;
-    }
-
-    public void setIdEstado(Long idEstado) {
-        this.idEstado = idEstado;
-    }
+    @OneToOne
+    private Status_bootcamps idEstado;
 }
