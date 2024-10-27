d7a2a54e55d8fb4b9bcf52c10132b02aeda1bd4a
kamilo9809 <camilorivera09@outlook.com>
2024-10-27 16:21:21 -0500

    feat: :sparkles: add ubications institutions controller

--- src/main/java/com/group_3/backen_project/controllers/ubications_institutions/Ubications_institutionsController.java
+++ src/main/java/com/group_3/backen_project/controllers/ubications_institutions/Ubications_institutionsController.java
@@ -1,5 +1,53 @@
 package com.group_3.backen_project.controllers.ubications_institutions;
 
-public class Ubications_institutionsController {
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.group_3.backen_project.controllers.ubications_institutions.services.Ubications_institutionsService;
+import com.group_3.backen_project.models.Ubications_institutions;
+
+
+@RestController
+@RequestMapping("api/ubications-institutions")
+public class Ubications_institutionsController {
+     @Autowired
+    private Ubications_institutionsService uInstitutionsService;
+
+    @PostMapping
+    public Ubications_institutions createUbication(@RequestBody Ubications_institutions ubication){
+        return uInstitutionsService.createUbication(ubication);
+    }
+
+    @GetMapping
+    public List<Ubications_institutions> getAllUbications() {
+        return uInstitutionsService.getAllUbications();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ubications_institutions> getUbicationById(@PathVariable Long id) {
+        Optional<Ubications_institutions> ubication = uInstitutionsService.getubicationById(id);
+        return ubication.map(ResponseEntity::ok).orElseGet(() ->
+                ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ubications_institutions> updateUbication(@PathVariable Long id, @RequestBody
+    Ubications_institutions detailsUbications) {
+        return ResponseEntity.ok(uInstitutionsService.updateUbications(id, detailsUbications));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUbication(@PathVariable Long id) {
+        uInstitutionsService.deleteUbication(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/group_3/backen_project/controllers/ubications_institutions/services/Ubications_institutionsService.java
+++ src/main/java/com/group_3/backen_project/controllers/ubications_institutions/services/Ubications_institutionsService.java
@@ -1,5 +1,36 @@
 package com.group_3.backen_project.controllers.ubications_institutions.services;
 
-public class Ubications_institutionsService {
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.group_3.backen_project.models.Ubications_institutions;
+import com.group_3.backen_project.repositories.UbicationsInstitutionsRepository;
+
+@Service
+public class Ubications_institutionsService {
+     @Autowired
+    private UbicationsInstitutionsRepository ubicationsInstitutionsRepository;
+
+    public Ubications_institutions createUbication(Ubications_institutions ubications){
+        return ubicationsInstitutionsRepository.save(ubications);
+    }
+
+    public Optional<Ubications_institutions> getubicationById(Long id) {
+        return ubicationsInstitutionsRepository.findById(id);
+    }
+
+    public List<Ubications_institutions> getAllUbications() {
+        return ubicationsInstitutionsRepository.findAll();
+    }
+
+    public Ubications_institutions updateUbications(Long id, Ubications_institutions detailsUbications) {
+        Ubications_institutions ubication = ubicationsInstitutionsRepository.findById(id).orElseThrow();
+        ubication.setZona(detailsUbications.getZona());
+        return ubicationsInstitutionsRepository.save(ubication);
+    }
+
+    public void deleteUbication(Long id) {
+        ubicationsInstitutionsRepository.deleteById(id);
+    }
 }
