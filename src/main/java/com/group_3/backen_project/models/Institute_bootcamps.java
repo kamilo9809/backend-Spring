0bf7f21132e65b5ff3fd5b99c4e3164cc4351533
kamilo9809 <camilorivera09@outlook.com>
2024-11-07 00:14:39 -0500

    feat: :sparkles: add entity institutions bootcamps

--- src/main/java/com/group_3/backen_project/models/Institute_bootcamps.java
+++ src/main/java/com/group_3/backen_project/models/Institute_bootcamps.java
@@ -8,4 +8,12 @@ public class Institute_bootcamps {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_institutions", referencedColumnName = "id")
+    private Institutions institutions;
+
+    @OneToMany
+    @JoinColumn(name = "id_bootcamps", referencedColumnName = "id")
+    private Bootcamps bootcamps; 
 }
