423797832c1ef4b545971cc0afb3e8baf82a4be2
kamilo9809 <camilorivera09@outlook.com>
2024-11-07 00:29:21 -0500

    feat: :sparkles: add entity Description bootcamp

--- src/main/java/com/group_3/backen_project/models/Descriptions_bootcamps.java
+++ src/main/java/com/group_3/backen_project/models/Descriptions_bootcamps.java
@@ -1,11 +1,15 @@
 package com.group_3.backen_project.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Descriptions_bootcamps {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String description;
 }
