f3dac07de578c4d57ce881d1590256d038f3cb92
Katherine Hurtado <katherine.hurtado@outlook.com>
2024-11-07 20:17:18 -0500

    feature/feature/entidad/relacional/usuarios_bootcamps/Entidad Usuarios_Bootcamp

--- src/main/java/com/group_3/backen_project/models/User_bootcamps.java
+++ src/main/java/com/group_3/backen_project/models/User_bootcamps.java
@@ -1,11 +1,22 @@
 package com.group_3.backen_project.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Table(name = "users bootcamps")
+@Data
 public class User_bootcamps {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", referencedColumnName = "id")
+    private Users idUsuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_bootcamp", referencedColumnName = "id")
+    private Bootcamps idBootcamp;
 }
