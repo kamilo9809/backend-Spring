610c53ac16c7ed337329158ee0b431577bbf225d
kamilo9809 <camilorivera09@outlook.com>
2024-11-11 20:33:48 -0500

    feat: :sparkles: add feature descriptions bootcamps

--- src/main/java/com/group_3/backen_project/controllers/description_bootcamps/description_bootcamps/Description_bootcampsService.java
+++ src/main/java/com/group_3/backen_project/controllers/description_bootcamps/description_bootcamps/Description_bootcampsService.java
@@ -1,5 +1,39 @@
 package com.group_3.backen_project.controllers.description_bootcamps.description_bootcamps;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.group_3.backen_project.models.Descriptions_bootcamps;
+import com.group_3.backen_project.repositories.DescriptionsBootcampsRepository;
+
+@Service
 public class Description_bootcampsService {
 
+    @Autowired
+    private DescriptionsBootcampsRepository descriptionsBootcampsRepository;
+
+     public Descriptions_bootcamps createDescriptions(Descriptions_bootcamps descriptionsBootcamps) {
+        return descriptionsBootcampsRepository.save(descriptionsBootcamps);
+    }
+
+    public Optional<Descriptions_bootcamps> getDescriptionById(Long id) {
+        return descriptionsBootcampsRepository.findById(id);
+    }
+
+    public List<Descriptions_bootcamps> getAllDescriptions() {
+        return descriptionsBootcampsRepository.findAll();
+    }
+
+    public Descriptions_bootcamps updateDescriptions(Long id, Descriptions_bootcamps detailDescriptions) {
+        Descriptions_bootcamps descriptionsBootcamps = descriptionsBootcampsRepository.findById(id).orElseThrow();
+        descriptionsBootcamps.setDescription(detailDescriptions.getDescription());
+        return descriptionsBootcampsRepository.save(descriptionsBootcamps);
+    }
+
+    public void deleteDescriptions(Long id) {
+        descriptionsBootcampsRepository.deleteById(id);
+    }
 }
