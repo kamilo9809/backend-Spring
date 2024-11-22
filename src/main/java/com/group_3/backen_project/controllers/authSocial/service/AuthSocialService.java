6c95dd41286435665badf529de030bbbcbb3f4bf
kamilo9809 <camilorivera09@outlook.com>
2024-11-21 23:38:54 -0500

    refactor: :recycle: refactor code

---
+++ src/main/java/com/group_3/backen_project/controllers/authSocial/service/AuthSocialService.java
@@ -0,0 +1,17 @@
+// package com.group_3.backen_project.controllers.authSocial.service;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+
+// public class AuthSocialService {
+    
+//     @Autowired
+//     private UserRepository userRepository;
+
+//     public Users createUser(AuthSocial authSocial){
+//         if(authSocial.token.empty()){
+//             return ;
+//         }
+//     }
+
+
+// }
---
+++ src/main/java/com/group_3/backen_project/dto/AuthSocial.java
@@ -0,0 +1,11 @@
+package com.group_3.backen_project.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthSocial {
+
+    private String token;
+    private String email;
+    private String nombre;
+}
